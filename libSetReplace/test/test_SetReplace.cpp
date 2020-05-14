906f0a16de53731b0bbe9910cc1d26a1c02d7208
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-05-14 14:57:51 +0200

    Remove SetReplaceIO from test_SetReplace

--- libSetReplace/test/test_SetReplace.cpp
+++ libSetReplace/test/test_SetReplace.cpp
@@ -4,16 +4,11 @@
 #include "Match.hpp"
 #include "Rule.hpp"
 #include "Set.hpp"
-#include "SetReplaceIO.hpp"
 
 using namespace SetReplace;
 auto & os = std::cout;
 
-TEST(SetReplace, gtest_works) {
-    os << "Hi SetReplace" << std::endl;
-}
-
-TEST(SetReplace, createSet) {
+TEST(SetReplace, createSetAndReplace) {
     // Negative atoms refer to patterns (useful for rules)
     std::vector<Rule> rules;
     auto a_rule = Rule({
@@ -23,8 +18,6 @@ TEST(SetReplace, createSet) {
     rules.push_back(a_rule);
 
     std::vector<AtomsVector> initialExpressions = {{1,2},{2, 3}};
-    os << "initialExpressions" << std::endl;
-    print(initialExpressions, os);
 
     Matcher::OrderingSpec orderingSpec = {
         {Matcher::OrderingFunction::SortedExpressionIDs,
@@ -40,22 +33,15 @@ TEST(SetReplace, createSet) {
 
     auto shouldAbort = [](){return false;};
     Set::StepSpecification stepSpec;
-    stepSpec.maxEvents = 100;
+    stepSpec.maxEvents = 10;
     stepSpec.maxGenerationsLocal = 100;
     stepSpec.maxFinalAtoms = 100000;
     stepSpec.maxFinalAtomDegree = 100000;
     stepSpec.maxFinalExpressions = 100000;
-    os << "StepSpecification:\n";
-    print(stepSpec, os);
     EXPECT_TRUE(a_set.replace(stepSpec, shouldAbort));
-    auto expressions = a_set.expressions();
-    for(auto exp : expressions) {
-        print(exp, os);
-        os << "---" << std::endl;
-    }
-    auto reason = a_set.terminationReason();
-    os << "TerminationReason: "; print(reason, os);
-    auto max_generation = a_set.maxCompleteGeneration(shouldAbort);
-    os << "MaxCompleteGeneration: " << max_generation << std::endl;
+    // auto expressions = a_set.expressions();
+    // auto reason = a_set.terminationReason();
+    // auto max_generation = a_set.maxCompleteGeneration(shouldAbort);
+    // os << "MaxCompleteGeneration: " << max_generation << std::endl;
 
 }
